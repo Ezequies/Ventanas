8863de976d0e3b9d91d090c8dad95e271f9e0d43
Ezequiel <lunaezequiel2@gmail.com>
2023-09-25 09:59:30 -0300

    create window Ok

---
+++ Ventanas/Program.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Window.h"
+
+
+
+int main()
+{
+    std::cout << "Creating Window\n";
+
+    Window* pWindow = new Window();
+
+    bool running = true;
+    while (running)
+    {
+        if (!pWindow->ProcessMessage())
+        {
+            std::cout << "Closing Window\n";
+            running = false;
+        }
+
+        //Render
+
+        Sleep(10);
+    }
+
+    delete pWindow;
+
+    return 0;
+}
